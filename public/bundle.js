4ae7c2f0f8055a753e11f851cf52a89bdb91a5ae
baturinsky <baturinsky@gmail.com>
2020-04-20 05:15:02 +0500

    more instruction editing

--- public/bundle.js
+++ public/bundle.js
@@ -4,10 +4,10 @@
   const lang = {
       instructions: `Goal of the game is to play all the cards in such an order that at least one alive cell is left.
 A card can kill some cells and create new ones, according to the rule written on the card.
-Each digit after the S on the card means that a living cell with as much living neighbors will live, otherwise it will die.
-Each digit after the B on the card means that in empty cell with as much living neighbors, a new living cell will appear.
+Each digit after the S on the card means that a living cell with exactly as much living neighbors will live, otherwise it will die.
+Each digit after the B on the card means that in empty cell with exactly as much living neighbors, a new living cell will appear.
 
-For example, B3/S23 means cell survives only with 2 or 3 neighbors and born when there is 3 neighbors.
+For example, B3/S23 means cell survives only with 2 or 3 neighbors, no more, no less; and born when there is exactly 3 neighbors.
 `
   };
 
